07459cca04f4b3c66080ef2c56b562556c20136d
Nocrex <20463817+Nocrex@users.noreply.github.com>
2026-04-11 18:51:55 +0200

    fix export logic

--- src/reports/exports.rs
+++ src/reports/exports.rs
@@ -1,10 +1,12 @@
+use std::{collections::HashSet, sync::Arc};
+
 use crate::{config::ExportConfig, reports::{PlayerReport, ReportDB}};
 use anyhow::Result;
 use itertools::Itertools;
 use tokio::fs;
 use serde_json::json;
 
-pub(crate) async fn export(db: &dyn ReportDB, cfg: &ExportConfig) -> Result<()> {
+pub(crate) async fn export(db: Arc<dyn ReportDB + Send + Sync>, cfg: &ExportConfig) -> Result<()> {
     let reports = db.all_reports().await?;
     simple_export(&reports, cfg).await?;
     tf2bd_export(&reports, cfg).await?;
@@ -12,8 +14,10 @@ pub(crate) async fn export(db: &dyn ReportDB, cfg: &ExportConfig) -> Result<()> 
 }
 
 async fn simple_export(reports: &Vec<PlayerReport>, cfg: &ExportConfig) -> Result<()> {
-    let content = reports.iter().filter(|r|r.verified).map(|r|r.steamid.steam64().to_string()).join("\n");
-    fs::write(&cfg.id_list_filename, content.as_bytes()).await?;
+    let mut content = reports.iter().filter(|r|r.verified).map(|r|r.steamid.steam64().to_string()).collect::<Vec<_>>();
+    content.sort();
+    content.dedup();
+    fs::write(&cfg.id_list_filename, content.into_iter().join("\n").as_bytes()).await?;
     Ok(())
 }
 
@@ -21,9 +25,9 @@ async fn tf2bd_export(reports: &Vec<PlayerReport>, cfg: &ExportConfig) -> Result
     
     let players = reports.iter().filter(|r|r.verified).into_group_map_by(|r|r.steamid).into_iter().map(|(id, reps)|{
         let steamid = id.steam3();
-        let attrs = reps.iter().map(|r|r.attribute.to_string()).collect::<Vec<_>>();
+        let attrs = reps.iter().map(|r|r.attribute.to_string()).collect::<HashSet<_>>();
         let proof = reps.iter().map(|r|&r.thread_url).collect::<Vec<_>>();
-        let last_seen = reps.iter().map(|r|r.last_seen).max().unwrap();
+        let last_seen = reps.iter().map(|r|r.last_seen).max().unwrap().timestamp();
         
         json!({
             "attributes": attrs,
@@ -37,7 +41,7 @@ async fn tf2bd_export(reports: &Vec<PlayerReport>, cfg: &ExportConfig) -> Result
     
     let now = chrono::Utc::now().format("%Y-%m-%d %H:%M:%S").to_string();
     let title = format!("vorobey-hackerpolice - {now}");
-    let description = "List of cheaters reported in the hackerpolice channel on the Vorobey discord server, last updated {now}";
+    let description = format!("List of cheaters reported in the hackerpolice channel on the Vorobey discord server, last updated {now}");
     
     let content = json!({
         "$schema": "https://raw.githubusercontent.com/PazerOP/tf2_bot_detector/master/schemas/v3/playerlist.schema.json",
