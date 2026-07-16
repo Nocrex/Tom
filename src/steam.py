7d48c098a797d85a10c7f7cfa001a5723e56d3f2
Nocrex <20463817+Nocrex@users.noreply.github.com>
2026-07-16 18:16:55 +0200

    swap vanity lookup to steam api

--- src/steam.py
+++ src/steam.py
@@ -5,7 +5,6 @@ import aiohttp
 
 log = logging.getLogger(__name__)
 
-STEAMID_XML_PATTERN = re.compile("<steamID64>(\\d+)</steamID64>")
 VANITY_LINK_PATTERN = re.compile("(https://steamcommunity.com/id/([\\w-]+))")
 PERM_LINK_PATTERN = re.compile(
     "https://(?:steamcommunity.com/profiles|steamhistory.net/id|shadefall.net/archive)/(\\d+)"
@@ -22,16 +21,20 @@ def sid3_to64(id: str) -> None | int:
     return int(res.group(1)) + 76561197960265728
 
 
+steam_token = open("steamtoken.txt").read().strip()
+
 async def resolve_vanity_url(url: str) -> int | None:
-    async with aiohttp.ClientSession() as session:
-        for _ in range(3):
-            async with session.get(url + "?xml=1") as resp:
-                txt = await resp.text()
-                steamid = STEAMID_XML_PATTERN.search(txt)
-                if steamid:
-                    return int(steamid.group(1))
-                if "The specified profile could not be found." in txt:
-                    return None
-                await asyncio.sleep(0.1)
-        log.error(f"Failed to resolve profile '{url}' after 3 tries")
+    vanity = VANITY_LINK_PATTERN.match(url)
+    if vanity is None:
         return None
+    vanity = vanity.group(2)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/?vanityurl={vanity}&key={steam_token}") as resp:
+            js = await resp.json()
+            status = js["response"]["success"]
+            if status == 1:
+                return int(js["response"]["steamid"])
+            if status == 42:
+                return None
+            log.error(f"Failed to resolve profile '{url}', response: \n{js}")
+            return None
